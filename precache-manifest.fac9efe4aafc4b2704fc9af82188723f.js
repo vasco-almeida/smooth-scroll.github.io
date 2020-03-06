8a4553e38bb8e9a257f9c161a15b6c060ed2541e
Vasco Almeida <castelobranco.vasco9@gmail.com>
2020-03-06 19:15:47 +0000

    First commit

---
+++ precache-manifest.fac9efe4aafc4b2704fc9af82188723f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2c2afd1537f4a52d99d584cc0e5efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d10007cc9fe406a56c",
+    "url": "/static/css/main.e73cb61d.chunk.css"
+  },
+  {
+    "revision": "6a0eedcc59974c6ee18f",
+    "url": "/static/js/2.15797cb7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.15797cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d10007cc9fe406a56c",
+    "url": "/static/js/main.e8fe9802.chunk.js"
+  },
+  {
+    "revision": "9fd909e13761795b3c69",
+    "url": "/static/js/runtime-main.8c316ab9.js"
+  }
+]);
